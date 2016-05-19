2db1f0c8437ce39ce47cb0036e937c39cf39a87b
Alex <blackkorol@gmail.com>
2016-05-20 01:12:00 +0300

    instapi with params uploadPhoto

---
+++ examples/uploadPhoto.php
@@ -0,0 +1,33 @@
+<?php
+
+require '../src/Instagram.php';
+
+/////// CONFIG ///////
+// $username = '4ewir';
+// $password = 'qweqwe';
+
+$username = $argv[1]
+$password = $argv[2]
+
+$debug = false;
+
+$photo = $argv[4];       // path to the photo
+$caption = $argv[3];     // caption
+//////////////////////
+
+$i = new Instagram($username, $password, $debug);
+
+try {
+    $i->login();
+} catch (InstagramException $e) {
+    $e->getMessage();
+    exit();
+}
+
+try {
+    $i->uploadPhoto($photo, $caption);
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+
