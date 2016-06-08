c4b9dc41cad2589d397a63922b8a85279f69789c
Alex <blackkorol@gmail.com>
2016-06-08 20:47:05 +0300

    added max_id and timer for post curl

--- examples/curl.php
+++ examples/curl.php
@@ -1,52 +1,128 @@
 <?php
-$handle = @fopen("/Users/alex/home/dev/rails/instagram/InstA/email_proxy/login_names", "r");
- $lines=array();
-if ($handle) {
-    while (($buffer = fgets($handle, 4096)) !== false) {
-        // echo $buffer;
+
+// $profileSetter = "1";
+// $dir    = '/Users/alex/home/dev/rails/instagram/InstAPI/src/'.$profileSetter;
+// $files1 = scandir($dir);
+// // $files2 = scandir($dir, 1);
+
+// // posts_per_day = 4
+//  // self.posts_per_day = posts_per_day
+//  //        if self.posts_per_day != 0:
+//  //            self.post_delay = self.time_in_day / self.posts_per_day
+
+// // add_time(post_delay)
+// // return post_delay*0.9 + post_delay*0.2*random.random()
+
+// // while true
+
+// foreach ( $files1 as $key => $value ) {
+    
+//     $ext = pathinfo($value, PATHINFO_EXTENSION);
+//     if ($ext == "jpg") {
+
+//         echo $value."\n";
+//     }
+
+// }
 
 
-    //add to array
-     $lines[]=trim($buffer);
+$json = '{
+    "status": "ok",
+    "big_list": true,
+    "users": [{
+        "username": "asl.mert",
+        "has_anonymous_profile_picture": true,
+        "profile_pic_url": "http://scontent-lhr3-1.cdninstagram.com/t51.2885-19/11906329_960233084022564_1448528159_a.jpg",
+        "full_name": "Asl\u0131 Mert",
+        "pk": 3294959691,
+        "is_verified": false,
+        "is_private": false
+    },{
+        "username": "ali_mashaikhi",
+        "has_anonymous_profile_picture": false,
+        "profile_pic_url": "http://scontent-lhr3-1.cdninstagram.com/t51.2885-19/s150x150/13116539_277661979235903_421638637_a.jpg",
+        "full_name": "~\u00b0\u25cf( \u0639\u0644\u064a \u0622\u0625\u0644\u0645\u0640\u2665\u0328\u0325\u032c\u0329\u0634\u0622\u0625\u064a\u062e\u064a )\u25cf\u00b0~",
+        "pk": 1269703933,
+        "is_verified": false,
+        "is_private": false
+    }],
+    "page_size": 200,
+    "next_max_id": "AQDoABpB7cRzDcTsdQeb7JDFAw-v_muGDyD7ljq72ujH6-ejagHcg4wdtQvx2hUs9by4NIxcV_i8kMRy1mfScjvh18axrt12ww8QIdQCCt7sba21LUsBdKh2vUX4bZhO700"
+  }';
+
+$obj = json_decode($json);
+
+$next_max_id = $obj->{'next_max_id'};
+
+echo $next_max_id;
+
+// $array = array(0 => 100, "color" => "red");
+// print_r(array_keys($array));
+
  
-    }
-    if (!feof($handle)) {
-        echo "Error: unexpected fgets() fail\n";
-    }
-    fclose($handle);
-}
  
-// echo implode ("\n",$lines);
-
-
-
-$i = 0; 
-
-while ($i < count($lines)){
-    $pieces = explode(" ", $lines[$i]);
+// $var = array_values($array);
  
-    if ($pieces[0] == "vildenilsen"){
-        $outarray = array_slice($lines, $i + 1);
-         $GLOBALS["lines"] = $outarray;
-        // $file = '/Users/alex/home/dev/rails/instagram/InstA/logs/regDone.dat';
-        // $person = "hanneaas93\n";
-        // file_put_contents($file, $person, FILE_APPEND | LOCK_EX);  
-        file_put_contents("/Users/alex/home/dev/rails/instagram/InstA/email_proxy/login_names", "");
-        file_put_contents("/Users/alex/home/dev/rails/instagram/InstA/email_proxy/login_names", implode("\n",$outarray));
+//  echo array_pop($var);
+
+
+
+
+// print_r($files1);
+// print_r($files2);
+ 
+
+
+
+
+// $handle = @fopen("/Users/alex/home/dev/rails/instagram/InstA/email_proxy/login_names", "r");
+//  $lines=array();
+// if ($handle) {
+//     while (($buffer = fgets($handle, 4096)) !== false) {
+//         // echo $buffer;
+
+
+//     //add to array
+//      $lines[]=trim($buffer);
+ 
+//     }
+//     if (!feof($handle)) {
+//         echo "Error: unexpected fgets() fail\n";
+//     }
+//     fclose($handle);
+// }
+ 
+// // echo implode ("\n",$lines);
+
+
+
+// $i = 0; 
+
+// while ($i < count($lines)){
+//     $pieces = explode(" ", $lines[$i]);
+ 
+//     if ($pieces[0] == "vildenilsen"){
+//         $outarray = array_slice($lines, $i + 1);
+//          $GLOBALS["lines"] = $outarray;
+//         // $file = '/Users/alex/home/dev/rails/instagram/InstA/logs/regDone.dat';
+//         // $person = "hanneaas93\n";
+//         // file_put_contents($file, $person, FILE_APPEND | LOCK_EX);  
+//         file_put_contents("/Users/alex/home/dev/rails/instagram/InstA/email_proxy/login_names", "");
+//         file_put_contents("/Users/alex/home/dev/rails/instagram/InstA/email_proxy/login_names", implode("\n",$outarray));
     
 
 
-        break;
-    }
-     echo $pieces[0]."\n";
-    $i  = $i + 1;
+//         break;
+//     }
+//      echo $pieces[0]."\n";
+//     $i  = $i + 1;
 
-}
+// }
 
 
 
  
-// $size = 640;
+// // $size = 640;
 // $raw = true;
 //  $file = "test.jpg";
 //  list($width, $height) = getimagesize($file);
--- examples/registrationToolOLD.php
+++ examples/registrationToolOLD.php
@@ -1,10 +1,14 @@
 <?php
 
-require_once '/root/instapi/src/InstagramRegistration.php';
+// require_once '/root/instapi/src/InstagramRegistration.php';
 
-require '/root/instapi/src/Instagram.php';
+// require '/root/instapi/src/Instagram.php';
 
  
+require_once '/Users/alex/home/dev/rails/instagram/InstAPI/src/InstagramRegistration.php';
+
+require '/Users/alex/home/dev/rails/instagram/InstAPI/src/Instagram.php';
+
 
 // NOTE: THIS IS A CLI TOOL
 /// DEBUG MODE ///
@@ -17,18 +21,19 @@ $password = $argv[1];
 $email= $argv[2]; 
 $url = $argv[3];  
 $biography = $argv[4];  
+// $caption = $argv[5];  
 $gender = 2;
 $phone  = "";
-$photo = "/root/instapi/src/".$argv[5];  
+$photo = "/Users/alex/home/dev/rails/instagram/InstAPI/src/".$argv[5];  
 
-$filePhoto = "/root/instapi/src/1/9.jpg";
-$filePhoto2 = "/root/instapi/src/1/16.jpg";
+$filePhoto = "/Users/alex/home/dev/rails/instagram/InstAPI/src/1/9.jpg";
+$filePhoto2 = "/Users/alex/home/dev/rails/instagram/InstAPI/src/1/16.jpg";
 $caption = "Wow) Do you like?";
 $caption2 = "Cool!";
 
 
 // READ LOGINS AND FIRST NAMES FROM FILE
-$login_names = @fopen("/root/insta/email_proxy/login_names", "r");
+$login_names = @fopen("/Users/alex/home/dev/rails/instagram/InstA/email_proxy/login_names", "r");
 $lines=array();
 if ($login_names) {
     while (($buffer = fgets($login_names, 4096)) !== false) {
@@ -40,7 +45,7 @@ if ($login_names) {
     fclose($login_names);
 }
 // READ PROXIES FROM FILE
-$proxy_list = @fopen("/root/insta/email_proxy/proxy_list", "r");
+$proxy_list = @fopen("/Users/alex/home/dev/rails/instagram/instA/email_proxy/proxy_list", "r");
 $prox=array();
 if ($proxy_list) {
     while (($buffer = fgets($proxy_list, 4096)) !== false) {
@@ -75,8 +80,8 @@ while ($p < count($prox))
 	    	$GLOBALS["first_name"] = $pieces[1]." ".$pieces[2];
 	    	$outar = array_slice($lines, $i+1);
 	    	$GLOBALS["lines"] = $outar;
-	    	file_put_contents("/root/insta/email_proxy/login_names", "");
-	    	file_put_contents("/root/insta/email_proxy/login_names", implode("\n",$outar));
+	    	file_put_contents("/Users/alex/home/dev/rails/instagram/InstA/email_proxy/login_names", "");
+	    	file_put_contents("/Users/alex/home/dev/rails/instagram/InstA/email_proxy/login_names", implode("\n",$outar));
 	    	
 	        break;
 	    }     
@@ -127,7 +132,7 @@ while ($p < count($prox))
 		sleep(8);
 		//upload photo
 		try {
-		    $i->uploadPhoto($filePhoto2, $caption2);
+		    $i->uploadPhoto($filePhoto2, $caption2); // use the same caption
 		} catch (Exception $e) {
 		    echo $e->getMessage();
 		}
@@ -144,21 +149,26 @@ while ($p < count($prox))
 		//     echo $e->getMessage();
 		// }
 		// sleep(4);
-		//// list of 200 followers need more
+		// // list of 200 followers need more
 		// try {
 		//   $followers = $i->getUserFollowers("13226335", $maxid = null);
 		//   $resfollowers=  var_export($followers);
 		//   echo $resfollowers;
+		// 	 $obj = json_decode($followers);
+		// 	 $next_max_id = $obj->{'next_max_id'};
+		// 	 $followers2 = $i->getUserFollowers("13226335", $next_max_id);
+		//   $resfollowers2=  var_export($followers2);
+		//   echo $resfollowers2;
 		// } catch (Exception $e) {
 		//     echo $e->getMessage();
 		// }
 
       $registered = $proxy." ".$username." ".$email." ".$password." ".$first_name."\n";
-      file_put_contents("/root/insta/logs/regDone.dat",$registered, FILE_APPEND | LOCK_EX);  
+      file_put_contents("/Users/alex/home/dev/rails/instagram/InstA/logs/regDone.dat",$registered, FILE_APPEND | LOCK_EX);  
 		$outarray = array_slice($prox, $p+1);
 		$GLOBALS["proxy_list"] = $outarray;
-		file_put_contents("/root/insta/email_proxy/proxy_list", "");
-		file_put_contents("/root/insta/email_proxy/proxy_list", implode("\n",$outarray));
+		file_put_contents("/Users/alex/home/dev/rails/instagram/InstA/email_proxy/proxy_list", "");
+		file_put_contents("/Users/alex/home/dev/rails/instagram/InstA/email_proxy/proxy_list", implode("\n",$outarray));
 	    break;
 	}
 	$p  = $p + 1;
