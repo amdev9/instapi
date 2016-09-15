b3033e6b86cfb034c359db3f48a3acf6e25641ee
Alexander Matveev <blackkorol@gmail.com>
2016-09-15 19:48:16 +0300

    c

--- examples/curl.php
+++ examples/curl.php
@@ -1,50 +1,75 @@
 <?php
  
 
-echo md5(uniqid(rand(), true));
+// echo md5(uniqid(rand(), true));
 
-echo microtime();
+// echo microtime();
 /* connect to gmail */
 $hostname = '{imap.gmail.com:993/imap/ssl}INBOX';
 $username = 'iprofilenumberqweqweqweqweqweq@gmail.com';
 $password = 'iprofilenumber';
 
+
+// $folders = imap_list($imap, "{imap.gmail.com:993/imap/ssl}", "*");
+// echo "<ul>";
+// foreach ($folders as $folder) {
+//     $folder = str_replace("{imap.gmail.com:993/imap/ssl}", "", imap_utf7_decode($folder));
+//     echo '<li><a href="mail.php?folder=' . $folder . '&func=view">' . $folder . '</a></li>';
+// }
+// echo "</ul>";
+
+
 /* try to connect */
 $inbox = imap_open($hostname,$username,$password); 
-// or die('Cannot connect to Gmail: ' . imap_last_error());
 
-/* grab emails */
-$emails = imap_search($inbox,'ALL');
+echo "fine";
 
-/* if emails are returned, cycle through each... */
-if($emails) {
-  
-  /* begin output var */
-  $output = '';
-  
-  /* put the newest emails on top */
-  rsort($emails);
-  
-  /* for every email... */
-  foreach($emails as $email_number) {
+
+$emails = imap_search($inbox,'RECENT');
+
+foreach($emails as $email_number) {
     
     /* get information specific to this email */
     $overview = imap_fetch_overview($inbox,$email_number,0);
     $message = imap_fetchbody($inbox,$email_number,2);
-    
-    /* output the email header information */
-    $output.= '<div class="toggler '.($overview[0]->seen ? 'read' : 'unread').'">';
-    $output.= '<span class="subject">'.$overview[0]->subject.'</span> ';
-    $output.= '<span class="from">'.$overview[0]->from.'</span>';
-    $output.= '<span class="date">on '.$overview[0]->date.'</span>';
-    $output.= '</div>';
-    
-    /* output the email body */
-    $output.= '<div class="body">'.$message.'</div>';
+    echo $message."\n";
+
   }
+
+// or die('Cannot connect to Gmail: ' . imap_last_error());
+
+// /* grab emails */
+// $emails = imap_search($inbox,'ALL');
+
+// /* if emails are returned, cycle through each... */
+// if($emails) {
   
-  echo $output;
-} 
+//   /* begin output var */
+//   $output = '';
+  
+//   /* put the newest emails on top */
+//   rsort($emails);
+  
+//   /* for every email... */
+//   foreach($emails as $email_number) {
+    
+//     /* get information specific to this email */
+//     $overview = imap_fetch_overview($inbox,$email_number,0);
+//     $message = imap_fetchbody($inbox,$email_number,2);
+    
+//     // output the email header information 
+//     $output.= '<div class="toggler '.($overview[0]->seen ? 'read' : 'unread').'">';
+//     $output.= '<span class="subject">'.$overview[0]->subject.'</span> ';
+//     $output.= '<span class="from">'.$overview[0]->from.'</span>';
+//     $output.= '<span class="date">on '.$overview[0]->date.'</span>';
+//     $output.= '</div>';
+    
+//     /* output the email body */
+//     $output.= '<div class="body">'.$message.'</div>';
+//   }
+  
+//   echo $output;
+// } 
 
 /* close the connection */
 imap_close($inbox);
