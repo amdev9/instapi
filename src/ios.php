25a3a6231546e0edc726680eddd83709b6bdf6a7
Alexander Matveev <blackkorol@gmail.com>
2016-11-17 12:01:34 +0300

    added algo to add points

--- src/ios.php
+++ src/ios.php
@@ -147,14 +147,39 @@ public function run() {
 
 public function edit_photo_tag($media_id, $removed_ids, $user_ids) {
 
+    $result_string_removed = "";
+    $result_string_added = "";
+   
+    foreach ($removed_ids as $removed_user_id ) {
+       $removed_user_string = '\"'. $removed_user_id .'\"'; 
+       $result_string_removed =  $result_string_removed . ",".$removed_user_string;
+    }
 
- 
+    $inter = 1;
+    $del = 5.1;
+    $interpol =  $inter / $del;
+
+    $counter = 1;
+    foreach ($user_ids as $user_id ) {
+
+      $y = $counter / 5;
+      $x = $counter - $y*5;
+      $x_pos = $x *  $interpol;
+      $y_pos = $y *  $interpol;
+
+      $added_user_string = '{\"user_id\":\"'. $user_id .'\",\"position\":['. $x_pos .','. $y_pos .']}'; 
+      $result_string_added =  $result_string_added . ",".$added_user_string;
+      $counter = $counter + 1;
+    }
+
+    $final_removed_string = '\"removed\":['.  $result_string_removed .']';
+    $final_added_string = '\"in\":['. $result_string_added .']';
+    $final_string =  "{".$final_removed_string.",".$final_added_string."}";
 
  // "usertags":"{\"removed\":[\"12335461\",\"49742317\"],\"in\":[{\"user_id\":\"230581164\",\"position\":[0.2374999970197678,0.3046875]}]}" 
 
 
-  $removed_user_string = '\"removed\":[\"'. $removed_user_id .'\"]'; 
-  $added_user_string = '{\"user_id\":\"'. $user_id .'\",\"position\":['. $x_pos .','. $y_pos .']}'; 
+ 
 
     $data = [ 
       "_csrftoken"  => $this->token,
