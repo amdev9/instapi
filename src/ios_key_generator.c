bbc0a7fd691833662c8c624ee0081d8497a3743d
Alexander Matveev <blackkorol@gmail.com>
2017-01-07 22:50:31 +0300

    fix generate key

--- src/ios_key_generator.c
+++ src/ios_key_generator.c
@@ -1,45 +1,38 @@
-#include <stdlib.h>
-#include <locale.h>
+ 
 #include <stdio.h>
-#include <string.h>
+ 
 
 int main()
 {
-    setlocale(LC_ALL, "en_US.utf8"); // or any other utf8 locale
-    wchar_t c = L'\x8C'; // or = L'\xc5';
-    char mb[MB_CUR_MAX + 1];
-    int len = wctomb(mb, c);
-    mb[len] = '\0';
-    printf("UTF-8 char: %s\n", mb);
 
-
-    char *p = "0x8C";
-	unsigned int intVal;
-	sscanf(p, "%x", &intVal);
-	printf("value x: %x - %d", intVal, intVal);
-
-	printf("\n");
-
+// input hex value
+// 86 D6 D6 C2 8B F3 EA F0  60 F3 7E 89 D6 F0 7E C2
+// C2 F0 EA C2 89 D6 D2 8B  7E 7E 8C F0 8C C2 C2 EA
+// 92 EA 8C C2 86 F0 6C D6  D2 92 6C D6 60 F3 D6 EA
+// 8C 86 6C 86 89 8B D6 6C  F0 C2 D2 89 7E 8B C2 F0
+// 00
+ 
 	// char *hex_str = "86D6D6C28BF3EAF060F37E89D6F07EC2C2F0EAC289D6D28B7E7E8CF08CC2C2EA92EA8CC286F06CD6D2926CD660F3D6EA8C866C86898BD66CF0C2D2897E8BC2F0";
+	// instagram 9.6
 	char *hex_str =    "86D6C2D68B8BF3EAF0608B7E89D2C2D28C929292EA8BF0606CD292B16C7E8C8CC28CEA6CF36CEA866C8BF086B1C56CC289C586B1C2F3EA7E8CF3898B7E6C8CD2";
-
-	char * res;
-	// int r[64];
-   
+	unsigned int intVal;
 	int s = 0;
 	int v2;
 	int v1;
 	for(int j = 0; j < 128; j = j + 2) {
 
-        sprintf(res, "%c%c", hex_str[j], hex_str[j+1] );
-    	// printf("%s ", res);
-   
-		sscanf(res, "%x", &intVal);
-		// printf(" %x - %d ", intVal, intVal);
+		char buffer [50];
+		sprintf (buffer, "%c%c", hex_str[j], hex_str[j+1]);
+		sscanf(buffer, "%x", &intVal);
+
+#ifdef DEBUG
+		printf("input hex: %x - %d ", intVal, intVal);
+#endif
 
 		v2  = intVal;
-	 	
-		    ////////
+
+		 
+
 		if ( v2 > 176 )
     {
       if ( v2 > 213 )
@@ -128,6 +121,14 @@ int main()
           break;
       }
     }
+
+
+
+///////////////////////////////////
+
+
+
+
 		    // if ( v2 > 193 )
 		    // {
 		    //   if ( v2 > 233 )
@@ -206,90 +207,20 @@ int main()
 		    //   }
 		    // }
 
-		  // printf("-> %d", v1);
+		
+#ifdef DEBUG
+		  printf("-> %d", v1);
+		  printf(" %c\n", (char)v1);
+#else
 		  printf("%c", (char)v1);
+#endif
 
 		s++;
 		 
     }
     // printf("%d\n", s);
 
-
+    return 0;
 }
 
-
-// input hex value
-// 86 D6 D6 C2 8B F3 EA F0  60 F3 7E 89 D6 F0 7E C2
-// C2 F0 EA C2 89 D6 D2 8B  7E 7E 8C F0 8C C2 C2 EA
-// 92 EA 8C C2 86 F0 6C D6  D2 92 6C D6 60 F3 D6 EA
-// 8C 86 6C 86 89 8B D6 6C  F0 C2 D2 89 7E 8B C2 F0
-// 00
-
-
-
-
-// \x86
-// \xD6
-// \xD6
-// \xC2
-// \x8B
-// \xF3
-// \xEA
-// \xF0
-// `
-// \xF3
-// ~
-// \x89
-// \xD6
-// \xF0
-// ~
-// \xC2
-// \xC2
-// \xF0
-// \xEA
-// \xC2
-// \x89
-// \xD6
-// \xD2
-// \x8B
-// ~
-// ~
-// \x8C
-// \xF0
-// \x8C
-// \xC2
-// \xC2
-// \xEA
-// \x92
-// \xEA
-// \x8C
-// \xC2
-// \x86
-// \xF0
-// l
-// \xD6
-// \xD2
-// \x92
-// l
-// \xD6
-// `
-// \xF3
-// \xD6
-// \xEA
-// \x8C
-// \x86
-// l
-// \x86
-// \x89
-// \x8B
-// \xD6
-// l
-// \xF0
-// \xC2
-// \xD2
-// \x89
-// ~
-// \x8B
-// \xC2
-// \xF0
 
